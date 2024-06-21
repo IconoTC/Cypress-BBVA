f01f1688325f5e706080a88dd9eaa39b4c1cce96
Javier Flores <javifb1983@gmail.com>
2024-06-21 13:46:09 +0200

    spec.cy

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,6 +2,12 @@
 
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://example.cypress.io');
+    cy.contains('.dropdown-menu', 'Traversal').should('not.be.visible');
+    cy.contains('Commands').click();
+    cy.contains('.dropdown-menu', 'Traversal').should('be.visible');
+    cy.get('.banner > .container').should('not.contain', 'Traversal');
+    cy.contains('Traversal').click();
+    cy.get('.banner > .container').should('contain', 'Traversal');
   })
 })
