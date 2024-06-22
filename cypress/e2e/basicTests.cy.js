d334608cde57552d7ccaa07b65b60673fa520803
Javier Flores <javifb1983@gmail.com>
2024-06-23 00:43:05 +0200

    cucumber, POM and accesibility

--- cypress/e2e/basicTests.cy.js
+++ cypress/e2e/basicTests.cy.js
@@ -4,7 +4,7 @@ describe('Basic tests', () => {
     cy.visit(URL);
   })
 
-  it.only('Click on button', () => {
+  it('Click on button', () => {
     cy.get('.component-container').first().should('not.contain', 'Button clicked');
     cy.get('.buttons-container').should('not.contain', 'Button clicked');
     cy.contains('Button clicked').should('not.exist');
@@ -38,7 +38,7 @@ describe('Basic tests', () => {
       cy.contains("Button right mouse clicked").should("exist"); 
       })
 
-    it.only('I can check radio button and check correct message appears', () =>{
+    it('I can check radio button and check correct message appears', () =>{
       cy.get('.radio-buttons-container').should('not.contain', 'option1 clicked')
       cy.get('.radio-buttons-container').should('not.contain', 'option2 clicked')
       cy.get('[data-testid = option1]').should('not.be.checked').check().should('be.checked');
@@ -50,13 +50,62 @@ describe('Basic tests', () => {
     })
 
     it('I can check radio button and check correct message appears', () =>{
-    cy.contains('option2 clicked').should('not.exist');
-    cy.contains('option1 clicked').should('not.exist');
-    cy.get('[data-testid="option1"]').click()
-    cy.contains('option1 clicked').should('exist');
-    cy.contains('option2 clicked').should('not.exist');
-    cy.get('[data-testid="option2"]').click()
-    cy.contains('option2 clicked').should('exist');
-    cy.contains('option1 clicked').should('not.exist');
+      cy.contains('option2 clicked').should('not.exist');
+      cy.contains('option1 clicked').should('not.exist');
+      cy.get('[data-testid="option1"]').click()
+      cy.contains('option1 clicked').should('exist');
+      cy.contains('option2 clicked').should('not.exist');
+      cy.get('[data-testid="option2"]').click()
+      cy.contains('option2 clicked').should('exist');
+      cy.contains('option1 clicked').should('not.exist');
     })
+
+// Nuevos tests a partir de aquí
+
+    it.only('Check select an option and check that the text appears inside selector', () => {
+      cy.get('select').contains('Select an option').should('exist').and('be.visible').and('have.attr', 'value', '');
+      cy.get('select option[value="option1"]').should('exist').and('be.visible').and('have.attr', 'value', 'option1');
+      cy.get('select option[value="option2"]').should('exist').and('be.visible').and('have.attr', 'value', 'option2');
+      cy.get('select option[value="option3"]').should('exist').and('be.visible').and('have.attr', 'value', 'option3');
+      cy.get('select option:selected').should('have.text', 'Select an option').and('have.attr', 'value', '');
+      cy.get('select').select('option1');
+      cy.get('select option:selected').should('have.text', 'Option 1').and('have.attr', 'value', 'option1');
+      cy.get('select').select('option2');
+      cy.get('select option:selected').should('have.text', 'Option 2').and('have.attr', 'value', 'option2');
+      cy.get('select').select('option3');
+      cy.get('select option:selected').should('have.text', 'Option 3').and('have.attr', 'value', 'option3');
+      cy.get('select').select('');
+      cy.get('select option:selected').should('have.text', 'Select an option');
   })
+
+  it('Check select multiple checkbox and check that the text appears inside selector', () => {
+      cy.get('input[data-testid="checkbox1"]').should('exist').and('be.visible').and('have.attr', 'name', 'checkbox1').and('have.attr', 'type', 'checkbox');
+      cy.get('input[data-testid="checkbox2"]').should('exist').and('be.visible').and('have.attr', 'name', 'checkbox2').and('have.attr', 'type', 'checkbox');
+      cy.get('input[data-testid="checkbox3"]').should('exist').and('be.visible').and('have.attr', 'name', 'checkbox3').and('have.attr', 'type', 'checkbox');
+      cy.contains('Checkbox 1 checked').should('not.exist');
+      cy.contains('Checkbox 2 checked').should('not.exist');
+      cy.contains('Checkbox 3 checked').should('not.exist');
+      cy.get('input[data-testid="checkbox1"]').check();
+      cy.get('input[data-testid="checkbox1"]').should('be.checked');
+      cy.get('input[data-testid="checkbox2"]').should('not.be.checked');
+      cy.get('input[data-testid="checkbox3"]').should('not.be.checked');
+      cy.contains('Checkbox 1 checked').should('exist').and('be.visible');
+      cy.contains('Checkbox 2 checked').should('not.exist');
+      cy.contains('Checkbox 3 checked').should('not.exist');
+      cy.get('input[data-testid="checkbox2"]').check();
+      cy.get('input[data-testid="checkbox1"]').should('be.checked');
+      cy.get('input[data-testid="checkbox2"]').should('be.checked');
+      cy.get('input[data-testid="checkbox3"]').should('not.be.checked');
+      cy.contains('Checkbox 1 checked').should('exist').and('be.visible');
+      cy.contains('Checkbox 2 checked').should('exist').and('be.visible');
+      cy.contains('Checkbox 3 checked').should('not.exist');
+      cy.get('input[data-testid="checkbox2"]').uncheck();
+      cy.get('input[data-testid="checkbox3"]').check();
+      cy.get('input[data-testid="checkbox1"]').should('be.checked');
+      cy.get('input[data-testid="checkbox2"]').should('not.be.checked');
+      cy.get('input[data-testid="checkbox3"]').should('be.checked');
+      cy.contains('Checkbox 1 checked').should('exist').and('be.visible');
+      cy.contains('Checkbox 2 checked').should('not.exist');
+      cy.contains('Checkbox 3 checked').should('exist').and('be.visible');
+  })
+})
